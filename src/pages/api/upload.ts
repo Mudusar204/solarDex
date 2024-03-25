1e1e981b4ceaee82189246ea87d73e1fe76fcf48
Abubaker <abubakeryounas6@gmail.com>
2024-03-25 21:17:53 +0500

    test

--- src/pages/api/upload.ts
+++ src/pages/api/upload.ts
@@ -10,53 +10,53 @@ export const config = {
 export default async function handler(req: any, res: any) {
   console.log("Request method:", req.method);
   // if (req.method === "POST") {
-    try {
-      const form: any = new IncomingForm();
-      form.keepExtensions = true;
-      form.parse(req, async (err: any, fields: any, files: any) => {
-        if (err) {
-          console.error("Error parsing form:", err);
-          return res
-            .status(500)
-            .json({ error: "Error parsing form data", status: false });
-        }
+  try {
+    const form: any = new IncomingForm();
+    form.keepExtensions = true;
+    form.parse(req, async (err: any, fields: any, files: any) => {
+      if (err) {
+        console.error("Error parsing form:", err);
+        return res
+          .status(500)
+          .json({ error: "Error parsing form data", status: false });
+      }
 
-        const { password }: any = fields;
-        const file: any = files?.file[0];
+      const { password }: any = fields;
+      const file: any = files?.file[0];
 
-        // Match the password against the environment keyword
-        const keyword: any = process.env.KEYWORD;
-        if (password != keyword) {
-          return res
-            .status(400)
-            .json({ error: "Invalid password keyword", status: false });
-        }
+      // Match the password against the environment keyword
+      const keyword: any = process.env.KEYWORD;
+      if (password != keyword) {
+        return res
+          .status(400)
+          .json({ error: "Invalid password keyword", status: false });
+      }
 
-        // Delete the existing wallets.csv file if it exists
-        const previousFilePath :string = path.join(
-          process.cwd(),
-          "src",
-          "wallets.csv"
+      // Delete the existing wallets.csv file if it exists
+      const previousFilePath: string = path.join(
+        process.cwd(),
+        "src",
+        "wallets.csv"
+      );
+      // if (fs.existsSync(previousFilePath)) {
+      //   fs.unlinkSync(previousFilePath);
+      // }
+      console.log("File path:", file);
+      try {
+        fs.writeFileSync(
+          path.join(process.cwd(), "src", "wallets.csv"),
+          fs.readFileSync(file._writeStream.path)
         );
-        if (fs.existsSync(previousFilePath)) {
-          fs.unlinkSync(previousFilePath);
-        }
-        console.log("File path:", file);
-        try {
-          fs.writeFileSync(
-            path.join(process.cwd(), "src", "wallets.csv"),
-            fs.readFileSync(file._writeStream.path)
-          );
-        } catch (e) {
-          console.log(e);
-        }
-        res
-          .status(200)
-          .json({ message: "File uploaded successfully", status: true });
-      });
-    } catch (e) {
-      console.log(e);
-    }
+      } catch (e) {
+        console.log(e);
+      }
+      res
+        .status(200)
+        .json({ message: "File uploaded successfully", status: true });
+    });
+  } catch (e) {
+    console.log(e);
+  }
   // } else {
   //   res.setHeader("Allow", ["POST"]);
   //   res.status(405).end(`Method ${req.method} Not Allowed`);
